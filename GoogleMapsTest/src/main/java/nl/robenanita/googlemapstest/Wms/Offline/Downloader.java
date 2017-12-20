6991f3781baf723a5fcd1d8f88fd297ab8d5a144
Mobile Aviation Tools <rob@robenanita.nl>
2017-12-20 17:15:25 +0100

    Added classes for offline downloading maps

---
+++ GoogleMapsTest/src/main/java/nl/robenanita/googlemapstest/Wms/Offline/Downloader.java
@@ -0,0 +1,116 @@
+package nl.robenanita.googlemapstest.Wms.Offline;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * Created by Rob Verhoef on 23-11-2016.
+ */
+
+public class Downloader extends AsyncTask {
+    public Downloader(Context context, Geometry buffer, String LocalPath, String Url, String Mapname)
+    {
+        this.buffer = buffer;
+        this.context = context;
+        localPath = LocalPath;
+        url = Url;
+        mapname = Mapname;
+    }
+
+    private Geometry buffer;
+    private Context context;
+    private String localPath;
+    private String mapname;
+    private String url;
+    private String TAG = "Downloader";
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        Tile tt = new Tile();
+
+        BoundingBox bb = new BoundingBox();
+
+        Coordinate[] testCoords = buffer.getEnvelope().getCoordinates();
+
+
+        int tileCount = 0;
+        for (int z = 6; z <= 13; z++) {
+            tt.getTileNumber(testCoords[1].y, testCoords[0].x, z);
+            int xBegin = tt.x;
+            int yBegin = tt.y;
+            tt.getTileNumber(testCoords[3].y, testCoords[2].x, z);
+            int xEnd = tt.x;
+            int yEnd = tt.y;
+
+            for (int x = xBegin; x<=xEnd; x++)
+            {
+                for (int y = yBegin; y<=yEnd; y++)
+                {
+                    bb.tile2boundingBox(x,y,z);
+                    if (buffer.intersects(bb.bbbox))
+                    {
+                        tileCount++;
+                    }
+                }
+
+            }
+
+        }
+
+        int downloadTileCount = 0;
+        for (int z = 6; z <= 13; z++) {
+            tt.getTileNumber(testCoords[1].y, testCoords[0].x, z);
+            int xBegin = tt.x;
+            int yBegin = tt.y;
+            tt.getTileNumber(testCoords[3].y, testCoords[2].x, z);
+            int xEnd = tt.x;
+            int yEnd = tt.y;
+
+            for (int x = xBegin; x<=xEnd; x++) {
+                for (int y = yBegin; y <= yEnd; y++) {
+                    bb.tile2boundingBox(x, y, z);
+                    if (buffer.intersects(bb.bbbox)) {
+                        Tile tile = new Tile(x, y, z);
+
+                        float percentage = ((float) downloadTileCount / (float) tileCount) * 100;
+
+                        Log.i(TAG, tile.Url() + "    : Percentage: " + Math.round(percentage));
+                        publishProgress(Math.round(percentage));
+
+                        tile.DownloadFile(localPath, mapname);
+                        downloadTileCount++;
+                    }
+//                    else
+//                    {
+//                        System.out.println(z + "/" + x + "/" + y + " is outside of buffer");
+//                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        Integer p = (Integer)values[0];
+        onDownloadProgress.OnProgress(p);
+        super.onProgressUpdate(values);
+    }
+
+    private OnDownloadProgress onDownloadProgress;
+    public void SetOnDownloadProgress(OnDownloadProgress d) { onDownloadProgress = d; }
+    public interface OnDownloadProgress
+    {
+        public void OnProgress(Integer progress);
+    }
+}
---
+++ GoogleMapsTest/src/main/java/nl/robenanita/googlemapstest/Wms/Offline/Tile.java
@@ -0,0 +1,69 @@
+package nl.robenanita.googlemapstest.Wms.Offline;
+
+import android.util.Log;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by Rob Verhoef on 23-11-2016.
+ */
+
+public class Tile {
+    public int x;
+    public int y;
+    public int z;
+    public Coordinate tile;
+
+    private String TAG = "TileDownload";
+
+    public Tile()
+    {
+
+    }
+    public Tile(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.tile = new Coordinate(this.x, this.y, this.z);
+    }
+
+    public void getTileNumber(final double lat, final double lon, final int zoom)
+    {
+        int xtile = (int)Math.floor( (lon + 180) / 360 * (1<<zoom) );
+        int ytile = (int)Math.floor( (1 - Math.log(Math.tan(Math.toRadians(lat)) + 1 / Math.cos(Math.toRadians(lat))) / Math.PI)  / 2 * (1<<zoom));
+
+        if (xtile < 0) xtile = 0;
+        if (xtile >= (1<<zoom)) xtile = ((1<<zoom) -1 );
+        if (ytile < 0) ytile = 0;
+        if (ytile >= (1<<zoom)) ytile = ((1<<zoom) - 1 );
+
+        this.x = xtile;
+        this.y = ytile;
+        this.z = zoom;
+
+        this.tile = new Coordinate(this.x, this.y, this.z);
+    }
+
+    public String Url()
+    {
+        return "http://tile.openstreetmap.org/" + z + "/" + x + "/" + y + ".png";
+    }
+
+    public void DownloadFile(String localPath, String baseName)
+    {
+        try {
+            String f = localPath + baseName + "/" + baseName + "-" + z + "-" + x + "-" + y + ".png";
+            Log.i(TAG, "Download from: " + Url() + " to: " + f );
+            FileUtils.copyURLToFile(new URL(Url()), new File(f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
