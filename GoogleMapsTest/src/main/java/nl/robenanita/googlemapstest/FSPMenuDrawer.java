4ada477a0d2eed013b7291c2e69a9d8ed2aa616d
Mobile Aviation Tools <rob@robenanita.nl>
2018-02-07 22:38:59 +0100

    work on new drawer menu

--- GoogleMapsTest/src/main/java/nl/robenanita/googlemapstest/FSPMenuDrawer.java
+++ GoogleMapsTest/src/main/java/nl/robenanita/googlemapstest/FSPMenuDrawer.java
@@ -2,6 +2,7 @@ package nl.robenanita.googlemapstest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
@@ -28,6 +29,8 @@ public class FSPMenuDrawer {
                 .withActivity(this.activity)
                 .withActionBarDrawerToggle(false)
                 .withActionBarDrawerToggleAnimated(true)
+                .withSliderBackgroundColor(Color.DKGRAY)
+                .withDisplayBelowStatusBar(true)
                 .build();
         createMenuItems();
         return drawer;
@@ -35,22 +38,44 @@ public class FSPMenuDrawer {
 
     private void createMenuItems()
     {
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(6).withName(R.string.action_directTo).withIcon(R.drawable.dirtecttobtn));
-
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(8).withName(R.string.action_flightplan_activate).withIcon(R.drawable.flightplanactivate));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(9).withName(R.string.action_connect_disconnect).withIcon(R.drawable.disconnected));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(10).withName(R.string.action_tracking_active).withIcon(R.drawable.trackactive));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(7).withName(R.string.action_flightplan).withIcon(R.drawable.flightplanbutton));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(6).withName(R.string.action_directTo)
+                .withIcon(R.drawable.dirtecttobtn)
+                .withTextColor(Color.LTGRAY)
+                .withSelectable(false));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(8).withName(R.string.action_flightplan_activate)
+                .withIcon(R.drawable.flightplanactivate)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(9).withName(R.string.action_connect_disconnect)
+                .withIcon(R.drawable.disconnected)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(10).withName(R.string.action_tracking_active)
+                .withIcon(R.drawable.trackactive)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(7).withName(R.string.action_flightplan)
+                .withIcon(R.drawable.flightplanbutton)
+                .withTextColor(Color.LTGRAY));
         drawer.addItem(new DividerDrawerItem());
 
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(11).withName(R.string.action_maptype).withIcon(R.drawable.maptype));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(5).withName(R.string.action_searchairports).withIcon(R.drawable.aerodromesearch));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(11).withName(R.string.action_maptype)
+                .withIcon(R.drawable.maptype)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(5).withName(R.string.action_searchairports)
+                .withIcon(R.drawable.aerodromesearch)
+                .withTextColor(Color.LTGRAY));
         drawer.addItem(new DividerDrawerItem());
 
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(1).withName(R.string.action_loadtrack));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(2).withName(R.string.action_settings));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(3).withName(R.string.action_load_test_chart));
-        drawer.addItem(new PrimaryDrawerItem().withIdentifier(4).withName(R.string.action_isnew));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(1)
+                .withName(R.string.action_loadtrack)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(2)
+                .withName(R.string.action_settings)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(3)
+                .withName(R.string.action_load_test_chart)
+                .withTextColor(Color.LTGRAY));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(4)
+                .withName(R.string.action_isnew)
+                .withTextColor(Color.LTGRAY));
         drawer.addItem(new DividerDrawerItem());
     }
 
