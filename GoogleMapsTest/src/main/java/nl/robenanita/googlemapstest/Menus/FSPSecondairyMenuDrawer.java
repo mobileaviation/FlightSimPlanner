94837c8f373ef020a554eb59094747a375a6750f
Mobile Aviation Tools <rob@robenanita.nl>
2018-02-15 22:32:16 +0100

    work on new menus

--- GoogleMapsTest/src/main/java/nl/robenanita/googlemapstest/Menus/FSPSecondairyMenuDrawer.java
+++ GoogleMapsTest/src/main/java/nl/robenanita/googlemapstest/Menus/FSPSecondairyMenuDrawer.java
@@ -7,7 +7,10 @@ import android.view.View;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import nl.robenanita.googlemapstest.R;
 
 /**
  * Created by Rob Verhoef on 15-2-2018.
@@ -43,7 +46,20 @@ public class FSPSecondairyMenuDrawer {
                     }
                 })
                 .append(drawer.getDrawer());
+        createMenuItems();
         return drawer.getDrawer();
+    }
+
+    private void createMenuItems() {
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(0).withName(R.string.action_airspace_check_active)
+                .withTag(MenuItemType.airspacesTracking)
+                .withTextColor(Color.LTGRAY)
+                .withSelectable(false));
+        drawer.addItem(new PrimaryDrawerItem().withIdentifier(0).withName(R.string.action_app_free)
+                .withTag(MenuItemType.appLocking)
+                .withIcon(R.drawable.unlocked)
+                .withTextColor(Color.LTGRAY)
+                .withSelectable(false));
     }
 
     public void setOnMenuTtemClicked(FSPMainMenuDrawer.OnMenuTtemClicked menuTtemClicked)
